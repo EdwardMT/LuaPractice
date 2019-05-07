055e809c9e5aae2af50f6e2aba225d0ab122736d
EdwardMT <edwardmt@outlook.com>
2019-05-07 13:36:09 +0800

    test anonymous function

--- test.lua
+++ test.lua
@@ -1,4 +1,5 @@
 -- function practice
+--[=[
 function factorial(n)
     if 0 == n then
         return 1
@@ -11,3 +12,18 @@ print(factorial(5))
 
 func = factorial
 print(func(5))
+]=]
+
+-- anonymous function
+function testFunc(tab, func)
+    for k, v in pairs(tab) do
+        print(func(k,v))
+    end
+end
+
+tab = {key1="val1", key2 = "val2"};
+testFunc(tab,
+    function(k, v)
+        return k.."="..v;
+    end
+);
