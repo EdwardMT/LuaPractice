0091cd2ac9bc999d3d53aa0282e6fc0fdb8237f8
EdwardMT <edwardmt@outlook.com>
2019-05-21 21:33:35 +0800

    函数作为参数传递

--- FuncTest.lua
+++ FuncTest.lua
@@ -8,3 +8,16 @@ function max(num1, num2)
 end
 
 print("max num is ", max(10, 5))
+
+local myprint = function(param)
+    print("my print func", param)
+end
+
+local function add(num1, num2, funcPrint)
+    local result = num1 + num2
+    funcPrint(result)
+end
+
+myprint(10)
+-- 函数作为参数传递
+add(2, 3, myprint)
